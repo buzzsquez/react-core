cec984c72ee871d0989fb5b1572fd4c223f1c391
buzzsquez <buzzsquez@gmail.com>
2022-10-03 14:11:54 +0300

    homework_4-6_todo_ts+tests_update

--- homework_1/homework_4/src/components/Button/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './style.module.css';
-
-function Button({ type }) {
-  return <button className={styles.save} type={type} aria-label="save name">Save</button>;
-}
-
-export default Button;
